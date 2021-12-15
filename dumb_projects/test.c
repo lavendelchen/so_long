0a857fe1fbdc67bb43d99ac8d0c053cd81c8f0f5
Lavendelchen <lavendel02sh@gmail.com>
2021-12-15 20:33:15 +0100

    tst

--- dumb_projects/test.c
+++ dumb_projects/test.c
@@ -6,14 +6,19 @@
 /*   By: shaas <shaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 01:50:33 by shaas             #+#    #+#             */
-/*   Updated: 2021/12/14 18:36:30 by shaas            ###   ########.fr       */
+/*   Updated: 2021/12/15 20:16:57 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <unistd.h>
-#include "mlx/mlx.h"
-#include "keys.h"
+#ifdef __APPLE__
+#include "mlx_mac/mlx.h"
+#endif
+#ifdef __linux__
+#include "mlx_linux/mlx.h"
+#endif
+#include "my_code/keys.h"
 #include <stdio.h>
 
 int	hook_do_something(int key, void *param)
@@ -50,15 +55,15 @@ void	make_rainbow(t_img_data	*img)
 	int	i = 1;
 	long long rainbow[6] = {0x00FF0018, 0x00FFA52C, 0x00FFFF41, 0x00008018, 0x000000F9, 0x0086007D};
 
-	int	format = 167;
+	int	format = 83;
 	while (i <= 6)
 	{
 		while (y < format)
 		{
-			while (x < 1000)
+			while (x < 500)
 			{
-				if ((y + (167 * (i - 1))) < 1000)
-					my_mlx_pixel_put(img, x, (y + (167 * (i - 1))), rainbow[i - 1]);
+				if ((y + (format * (i - 1))) < 500)
+					my_mlx_pixel_put(img, x, (y + (format * (i - 1))), rainbow[i - 1]);
 				x++;
 			}
 			x = 0;
@@ -76,16 +81,16 @@ int	main(void)
 	t_img_data	img;
 
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "Hello world!");
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hello world!");
 
 	//making the image of the rainbow
-	img.img_ptr = mlx_new_image(mlx_ptr, 1000, 1000);
+	img.img_ptr = mlx_new_image(mlx_ptr, 500, 500);
 	img.addr = mlx_get_data_addr(img.img_ptr, &img.bits_per_pixel, &img.size_line, &img.endian);
 	printf("address: [%s]\nbits per pixel: [%i]\nsize line: [%i]\nendian: [%i]\n", img.addr, img.bits_per_pixel, img.size_line, img.endian);
 	make_rainbow(&img);
 	mlx_put_image_to_window(mlx_ptr, win_ptr, img.img_ptr, 0, 0);
 
-	mlx_string_put(mlx_ptr, win_ptr, 450, 450, 0x00000000, "what's up homosexuals");
+	mlx_string_put(mlx_ptr, win_ptr, 100, 100, 0x00000000, "what's up homosexuals");
 
 	mlx_key_hook(win_ptr, hook_do_something, NULL);
 
@@ -113,4 +118,7 @@ int	main(void)
 //	mlx_destroy(mlx_ptr);
 //}
 
-// comp -Lmlx -lmlx -framework OpenGL -framework AppKit test.c
+// comp -Lmlx_mac -lmlx_mac -framework OpenGL -framework AppKit test.c
+
+// comp -l/usr/include -lmlx_linux -O3 -c test.c -o test.o
+// comp test.o -Lmlx_linux -lmlx_linux -L/usr/lib -lmlx_linux -lXext -lX11 -lm -lz
